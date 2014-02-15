186379a11fb08f5ae16b69622c51db3fc049a2d3
Florent Georges <fgeorges@gmail.com>
2014-02-15 17:00:22 +0100

    Embed external packages used in the sandbox.

--- sandbox/install.sh
+++ sandbox/install.sh
@@ -21,21 +21,21 @@ xrepo create "$BASEDIR/repo" \
     || die "Error creating the repo"
 
 xrepo --repo "$BASEDIR/repo" install \
-    "$BASEDIR/../../../../xlibs/fxsl/build/fxsl-1.0.xar" \
+    "$BASEDIR/packages/fxsl-1.0.xar" \
     || die "Error installing the FXSL library"
 
 xrepo --repo "$BASEDIR/repo" install \
-    "$BASEDIR/../../../../xlibs/serial/dist/serial-0.4.0pre2.xar" \
+    "$BASEDIR/packages/serial-0.4.0pre2.xar" \
     || die "Error installing the Serial library"
 
 xrepo --repo "$BASEDIR/repo" install \
-    "$BASEDIR/../../../crypto/saxon/build/expath-crypto-saxon-0.3.0.xar" \
+    "$BASEDIR/packages/expath-crypto-saxon-0.3.0.xar" \
     || die "Error installing EXPath Crypto for Saxon"
 
 xrepo --repo "$BASEDIR/repo" install \
-    "$BASEDIR/../../../http-client/saxon/build/expath-http-client-saxon-0.11.0dev.xar" \
+    "$BASEDIR/packages/expath-http-client-saxon-0.11.0dev.xar" \
     || die "Error installing EXPath HTTP Client for Saxon"
 
 xrepo --repo "$BASEDIR/repo" install \
-    "$BASEDIR/../website/dist/cxan-website-0.5.0dev.xaw" \
+    "$BASEDIR/../website/dist/cxan-website-0.6.0dev.xaw" \
     || die "Error installing CXAN Website"
